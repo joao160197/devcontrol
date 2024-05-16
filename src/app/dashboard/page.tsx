699d9eacac9f6974a903bdeca9c49bf236572e9e
unknown <ramoslopesjoaopaulo@gmail.com>
2024-05-16 20:00:52 -0300

    updating

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -19,30 +19,30 @@ export default async function Dashboard() {
     where: {
       status: "ABERTO",
       customer: {
-        userId: session.user.id
-      }
+        userId: session.user.id,
+      },
     },
     include: {
       customer: true,
     },
     orderBy: {
-      created_at: "asc"
-    }
-  })
+      created_at: "asc",
+    },
+  });
 
   return (
     <Container>
       <div className="w-full flex items-center justify-between mt-9 mb-2">
         <h1 className="text-black font-bold text-3xl">Chamadas</h1>
-       <div className="flex items-center gap-4">
-        <Refresh/>
-       <Link
-          className="bg-green-400 p-2 rounded  hover:bg-green-500 duration-500"
-          href="/dashboard/new"
-        >
-          Abrir Chamados
-        </Link>
-       </div>
+        <div className="flex items-center gap-4">
+          <Refresh />
+          <Link
+            className="bg-green-400 p-2 rounded  hover:bg-green-500 duration-500"
+            href="/dashboard/new"
+          >
+            Abrir Chamados
+          </Link>
+        </div>
       </div>
       <table className="min-w-full my-5">
         <thead>
@@ -54,17 +54,23 @@ export default async function Dashboard() {
           </tr>
         </thead>
         <tbody>
-          {tickets.map( ticket => (
+          {tickets.map((ticket) => (
             <Ticket
               key={ticket.id}
               customer={ticket.customer}
-              ticket={ticket}
+              ticket={
+                ticket.customer
+                  ? { ...ticket, customerId: ticket.customer.userId || "" }
+                  : { ...ticket, customerId: "" }
+              }
             />
           ))}
         </tbody>
       </table>
       {tickets.length === 0 && (
-        <h1 className="flex items-center ml-1 mt-2 md:px-0">Nenhum chamado criado.....</h1>
+        <h1 className="flex items-center ml-1 mt-2 md:px-0">
+          Nenhum chamado criado.....
+        </h1>
       )}
     </Container>
   );
