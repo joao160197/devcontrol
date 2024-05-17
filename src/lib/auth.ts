689b116a47b90c2027147d63f8e8473deedc5606
unknown <ramoslopesjoaopaulo@gmail.com>
2024-05-17 14:17:59 -0300

    updating

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,27 +1,14 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import GoogleProvider from 'next-auth/providers/google'
-import { AuthOptions } from 'next-auth'
-import prismaClient from './prisma'
+import NextAuth from "next-auth/next";
+import GoogleProcider from "next-auth/providers/google";
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prismaClient),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ],
-  callbacks: {
-    async session({ session, token, user, }){
-      session.user = { ...session.user, id: user.id } as {
-        id: string,
-        name: string;
-        email: string;
-      }
-
-
-      return session;
-
-    }
-  }
+export const authOptions = {
+    providers: [
+        GoogleProcider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ],
+    secret: process.env.JWT_SECRET as string,
 }
+
+export default NextAuth(authOptions);
